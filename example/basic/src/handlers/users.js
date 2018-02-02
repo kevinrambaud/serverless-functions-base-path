0e65e1a39590661765aeb90d4468a6c8e84ae4b0
Kevin Rambaud <kevin.rambaud@gmail.com>
2018-02-02 15:05:15 +0100

    Fix basic example

--- example/basic/src/handlers/users.js
+++ example/basic/src/handlers/users.js
@@ -1,11 +1,8 @@
 // users.js
 
 module.exports.hello = (event, context, callback) => {
-  callback(
-    null,
-    JSON.stringify({
-      statusCode: 200,
-      body: { message: 'Hello world!' }
-    })
-  );
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello world!' })
+  });
 };
